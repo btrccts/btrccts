d9323f5cbe3cc62193de1ea819715bf1b723eae0
brandsimon <simon.brand@postadigitale.de>
2019-09-02 11:26:44 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# sccts
+
+Simulate CryptoCurrency Trading Strategies
+
+## Development setup
+
+Setup a virtualenv:
+
+    python3 -m venv .venv
+    # TODO: provide requirements file with hashes
+    .venv/bin/pip install -e .
+
+## Run tests
+
+    .venv/bin/python -m unittest tests/unittests.py
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sccts',
+    version='0.0.1',
+    description='Simulate CryptoCurrency Trading Strategies',
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    author='Simon Brand',
+    author_email='simon.brand@postadigitale.de',
+    url='',
+    keywords='sccts',
+    package_dir={'': 'src'},
+    packages=find_packages('src/'),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+    },
+    install_requires=['ccxt'],
+    entry_points={
+    },
+)
