002ae5ea94178df16e09c58c49e50f0ab801602c
Simon Brand <simon.brand@postadigitale.de>
2020-02-15 11:47:22 +0000

    Add README algo to examples

---
+++ examples/algo_readme.py
@@ -0,0 +1,71 @@
+from btrccts import parse_params_and_execute_algorithm, AlgorithmBase
+
+
+class Algorithm(AlgorithmBase):
+
+    @staticmethod
+    def configure_argparser(argparser):
+        # Here you can add additional arguments to the argparser
+        argparser.add_argument('--pyramiding', default=1, type=int)
+
+    def __init__(self, context, args):
+        # Context is used to create exchanges or get the current time
+        self._context = context
+        self._args = args
+
+        # This will create a kraken exchange instance
+        # The interace in backtesting and live mode is identical to CCXT.
+        # See: [CCXT](https://github.com/ccxt/ccxt/wiki)
+        # In live mode, this will be a plain ccxt instance of the exchange
+        # The exchange keys will be read from the config directory (see --help)
+        self._kraken = context.create_exchange('kraken')
+
+        # You can access your own defined parameters
+        print('Pyramiding:', args.pyramiding)
+
+    def next_iteration(self):
+        # This method is executed each time interval
+
+        # This is the current context date:
+        print('context date', self._context.date())
+
+        # Use the exchange to load OHLCV data
+        ohlcv_len = 10
+        ohlcv_offset = ohlcv_len * 60 * 1000
+        ohlcv_start = int(self._context.date().value / 1000000 - ohlcv_offset)
+        print(self._kraken.fetch_ohlcv(
+            'BTC/USD', '1m', ohlcv_start, ohlcv_len))
+
+        # Use the exchange to create a market order
+        self._order_id = self._kraken.create_order(
+            type='market', side='buy', symbol='BTC/USD', amount=0.1)
+
+        # If you want to stop the algorithm in context or live mode, you can
+        # do this:
+        self._context.stop('stop message')
+
+    def handle_exception(self, e):
+        # This method is called, when next_iteration raises an exception, e.g.
+        # because of an exchange error or a programming error.
+        # If this method raises an exception, the algorith will stop with
+        # reason EXCEPTION
+        # If you are not in live mode, it is advicable to rethrow the
+        # exception to fix the programming error.
+        print(e)
+        if not self._args.live:
+            raise e
+
+    def exit(self, reason):
+        # This method is called, when the algorithm exits and should be used
+        # to cleanup (e.g. cancel open orders).
+        # reason contains information on why the algorithm exits.
+        # e.g. STOPPED, EXCEPTION, FINISHED
+        print("Done", reason)
+
+
+# This method parses commandline parameters (see --help)
+# and runs the Algorithm according to the parameters
+result = parse_params_and_execute_algorithm(Algorithm)
+# The result is an instance of Algorithm, you can now use saved
+# information or exchanges to benchmark your performance.
+print(result._kraken.fetch_closed_orders())
