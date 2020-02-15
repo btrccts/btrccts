62aa981f4101de0200852edbca94bc0a1242ffc8
Simon Brand <simon.brand@postadigitale.de>
2020-02-15 23:08:40 +0000

    setup.py: add long description

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
 
 setup(
     name='btrccts',
     version='0.0.1',
     description='BackTest and Run CryptoCurrency Trading Strategies',
+    long_description=readme,
     classifiers=[
         'Programming Language :: Python',
         'Intended Audience :: Developers',
