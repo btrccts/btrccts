bd42bcb17317388327d2f042cfdc0f439382d617
Simon Brand <simon.brand@postadigitale.de>
2020-02-20 18:01:45 +0000

    Release 0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='btrccts',
-    version='0.0.4',
+    version='0.0.5',
     description='BackTest and Run CryptoCurrency Trading Strategies',
     long_description=readme,
     long_description_content_type='text/markdown',
