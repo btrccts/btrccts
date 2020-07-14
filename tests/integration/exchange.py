2ae8843bbda02609bb44afbd9c2557965cc219d6
Simon Brand <simon.brand@postadigitale.de>
2020-07-14 13:03:10 +0000

    Make integration tests more robust by removing the ccxt info object before the comparison

--- tests/integration/exchange.py
+++ tests/integration/exchange.py
@@ -9,40 +9,6 @@ KRAKEN_BTC_USD = {
     'baseId': 'XXBT',
     'darkpool': False,
     'id': 'XXBTZUSD',
-    'info': {
-        'aclass_base': 'currency',
-        'aclass_quote': 'currency',
-        'altname': 'XBTUSD',
-        'base': 'XXBT',
-        'fee_volume_currency': 'ZUSD',
-        'fees': [[0, 0.26],
-                 [50000, 0.24],
-                 [100000, 0.22],
-                 [250000, 0.2],
-                 [500000, 0.18],
-                 [1000000, 0.16],
-                 [2500000, 0.14],
-                 [5000000, 0.12],
-                 [10000000, 0.1]],
-        'fees_maker': [[0, 0.16],
-                       [50000, 0.14],
-                       [100000, 0.12],
-                       [250000, 0.1],
-                       [500000, 0.08],
-                       [1000000, 0.06],
-                       [2500000, 0.04],
-                       [5000000, 0.02],
-                       [10000000, 0]],
-        'leverage_buy': [2, 3, 4, 5],
-        'leverage_sell': [2, 3, 4, 5],
-        'lot': 'unit',
-        'lot_decimals': 8,
-        'lot_multiplier': 1,
-        'margin_call': 80,
-        'margin_stop': 40,
-        'pair_decimals': 1,
-        'quote': 'ZUSD',
-        'wsname': 'XBT/USD'},
     'limits': {'amount': {'max': 100000000.0, 'min': 0.002},
                'cost': {'max': None, 'min': 0},
                'price': {'max': None, 'min': 0.1}},
@@ -58,10 +24,6 @@ KRAKEN_BTC_CURRENCY = {
     'code': 'BTC',
     'fee': None,
     'id': 'XXBT',
-    'info': {'aclass': 'currency',
-             'altname': 'XBT',
-             'decimals': 10,
-             'display_decimals': 5},
     'limits': {'amount': {'max': 10000000000.0, 'min': 1e-10},
                'cost': {'max': None, 'min': None},
                'price': {'max': 10000000000.0, 'min': 1e-10},
@@ -103,14 +65,19 @@ class BacktestExchangeBaseIntegrationTest(unittest.TestCase):
         kraken = self.backtest.create_exchange('kraken')
         markets_list = kraken.fetch_markets()
         market = list(filter(lambda x: x['symbol'] == 'BTC/USD', markets_list))
+        del market[0]['info']
         self.assertEqual(market, [KRAKEN_BTC_USD])
 
     def test__fetch_currencies(self):
         kraken = self.backtest.create_exchange('kraken')
-        self.assertEqual(kraken.fetch_currencies()['BTC'], KRAKEN_BTC_CURRENCY)
+        currencies = kraken.fetch_currencies()
+        del currencies['BTC']['info']
+        self.assertEqual(currencies['BTC'], KRAKEN_BTC_CURRENCY)
 
     def test__load_markets(self):
         kraken = self.backtest.create_exchange('kraken')
         kraken.load_markets()
+        del kraken.markets['BTC/USD']['info']
+        del kraken.currencies['BTC']['info']
         self.assertEqual(kraken.currencies['BTC'], KRAKEN_BTC_CURRENCY)
         self.assertEqual(kraken.markets['BTC/USD'], KRAKEN_MARKET_BTC_USD)
