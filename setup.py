e12db9d5457c5df7ad95ec115dc86eaf487041b0
Simon Brand <simon.brand@postadigitale.de>
2023-01-28 20:38:59 +0000

    Increase version to 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='btrccts',
-    version='0.1.2',
+    version='0.1.3',
     description='BackTest and Run CryptoCurrency Trading Strategies',
     long_description=readme,
     long_description_content_type='text/markdown',
